141c76517269d1fc72587c0b4c2c0e8fa1908e22
hugo vilar <hugovilar04@gmail.com>
2025-07-14 09:12:38 -0300

    feat: adicionando dotenv para variaveis de ambiente e seed do banco de dados

--- server/src/env.ts
+++ server/src/env.ts
@@ -1,8 +1,9 @@
+import 'dotenv/config' 
 import { z } from 'zod';
 
 const envSchema = z.object({
     PORT: z.coerce.number().default(3333),
-    DATABASE_URL: z.string().url()
+    DATABASE_URL: z.string().url().startsWith('postgresql://')
 });
 
 export const env = envSchema.parse(process.env);
