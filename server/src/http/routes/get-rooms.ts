70749fe3c902abb9a167f8eefd94896876f1b8d8
hugo vilar <hugovilar04@gmail.com>
2025-07-15 08:47:46 -0300

    feat: add route to fetch rooms from the database

---
+++ server/src/http/routes/get-rooms.ts
@@ -0,0 +1,14 @@
+import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod";
+import { db } from "../../db/connection.ts";
+import { schema } from "../../db/schema/index.ts";
+
+export const getRoomsRoute: FastifyPluginCallbackZod = async (app) => {
+    app.get('/rooms', async () => {
+        const results = await db.select({
+            id: schema.rooms.id,
+            name: schema.rooms.name,
+        }).from(schema.rooms).orderBy(schema.rooms.createdAt);
+
+        return results
+    })
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,7 @@ import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
 import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from 'fastify-type-provider-zod'
 import { env } from './env.ts'
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -12,6 +13,8 @@ app.register(fastifyCors, {
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
+
+app.register(getRoomsRoute)
 
 app.listen({ port: env.PORT }).then(() => {
     console.log(`LETS GOU is running on http://localhost:${env.PORT}`)
